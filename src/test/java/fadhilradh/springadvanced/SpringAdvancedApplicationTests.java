efb56b47b74ac99719aaf4b9899ac97d0b0c2f49
Fadhil Radhian <fadhilradhian@gmail.com>
2023-04-05 13:49:25 +0700

    add testing

--- src/test/java/fadhilradh/springadvanced/SpringAdvancedApplicationTests.java
+++ src/test/java/fadhilradh/springadvanced/SpringAdvancedApplicationTests.java
@@ -1,5 +1,6 @@
 package fadhilradh.springadvanced;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,7 @@ class SpringAdvancedApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
